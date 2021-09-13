dc908901a8752b4d12ef235b5c603f318553021e
nadeemcite <nady5730642@gmail.com>
2021-09-13 13:32:10 +0530

    minor

--- init.js
+++ init.js
@@ -1,5 +1,17 @@
 module.exports = {
-    sum() {
+  sum(a, b) {
+    if (Number(a) && Number(b)) {
+      return a + b;
+    } else {
+      throw "Error";
     }
-  };
-  
+  },
+
+  ci(p, r, t) {
+    const amount = p * Math.pow(1 + r / 12 / 100, t);
+    return {
+      interest: amount - p,
+      amount,
+    };
+  },
+};
